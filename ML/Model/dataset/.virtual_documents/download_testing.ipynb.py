a7fa839abcd7c6400adc97d741a6b47dce4eced5
Programmer-RD-AI <go2ranuga@gmail.com>
2022-04-26 21:44:02 +0530

    Update
    Co-Authored-By: Ranuga-Disansa <79456372+Programmer-RD-AI@users.noreply.github.com>

---
+++ ML/Model/dataset/.virtual_documents/download_testing.ipynb.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+
+links = pd.read_csv('./raw/links.csv')
+movies = pd.read_csv('./raw/movies.csv')
+ratings = pd.read_csv('./raw/ratings.csv')
+tags = pd.read_csv('./raw/tags.csv')
+
+
+links.head()
+
+
+movies.head()
+
+
+ratings.head()
+
+
+tags.head()
+
+
+
